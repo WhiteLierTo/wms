1b55c5d1e42af73d2b087995d1e9f4165e81ba8d
lijie <1262001568@qq.com>
2019-07-18 14:30:16 +0800

    fix：修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,90 +6,109 @@ Vue.use(Router)
 /* Layout */
 import Layout from '@/layout'
 
-export const constantRoutes = [
-  {
-    path: '/404',
+export const constantRoutes = [{
+  path: '/login',
+  component: () =>
+            import('@/views/login/index'),
+  hidden: true
+},
+
+{
+  path: '/404',
+  component: () =>
+            import('@/views/404'),
+  hidden: true
+},
+
+{
+  path: '/',
+  component: Layout,
+  redirect: '/home',
+  children: [{
+    path: 'home',
+    name: 'Home',
     component: () =>
-      import('@/views/home/index'),
+                import('@/views/home/index'),
     hidden: true,
     meta: { title: '首页', icon: 'home' }
+  }]
+},
+{
+  path: '/data-manager',
+  component: Layout,
+  redirect: '/data-manager/warehouse-manager',
+  name: 'data-manager',
+  meta: {
+    title: '数据管理',
+    icon: 'nested'
   },
-  {
-    path: '/data-manager',
-    component: Layout,
-    redirect: '/data-manager/warehouse-manager',
-    name: 'data-manager',
-    meta: {
-      title: '数据管理',
-      icon: 'nested'
-    },
-    children: [{
-      path: 'warehouse-manager',
-      component: () =>
-      import('@/views/data-manager/warehouse-manager'),
-      name: 'warehouse-manager',
-      meta: { title: '库房管理' },
-      children: []
-    }, {
-      path: 'location-manager',
-      component: () =>
-      import('@/views/data-manager/location-manager'),
-      name: 'location-manager',
-      meta: { title: '库位管理' },
-      children: []
-    }, {
-      path: 'production-batch',
-      component: () =>
-      import('@/views/data-manager/production-batch'),
-      name: 'production-batch',
-      meta: { title: '生产批次' },
-      children: []
-    }]
-  },
-  {
-    path: '/warehouse',
-    component: Layout,
-    children: [{
-      path: 'warehouse',
-      name: 'warehouse',
-      component: () =>
-            import('@/views/data-manager/warehouse-manager'),
-      meta: { title: '采购退货', icon: 'example', noCache: false, affix: true }
-    }]
-  },
+  children: [{
+    path: 'warehouse-manager',
+    component: () =>
+                import('@/views/data-manager/warehouse-manager'),
+    name: 'warehouse-manager',
+    meta: { title: '库房管理' },
+    children: []
+  }, {
+    path: 'location-manager',
+    component: () =>
+                import('@/views/data-manager/location-manager'),
+    name: 'location-manager',
+    meta: { title: '库位管理' },
+    children: []
+  }, {
+    path: 'production-batch',
+    component: () =>
+                import('@/views/data-manager/production-batch'),
+    name: 'production-batch',
+    meta: { title: '生产批次' },
+    children: []
+  }]
+},
+{
+  path: '/warehouse-manager',
+  component: Layout,
+  children: [{
+    path: 'warehouse-manager',
+    name: 'warehouse-manager',
+    component: () =>
+                import('@/views/data-manager/warehouse-manager'),
+    meta: { title: '采购退货', icon: 'example', noCache: false, affix: true }
+  }]
+},
 
-  {
-    path: '/form',
-    component: Layout,
-    children: [{
-      path: 'index',
-      name: 'Form',
-      component: () =>
-            import('@/views/form/index'),
-      meta: { title: '条码打印', icon: 'form' }
-    }]
-  },
+{
+  path: '/form',
+  component: Layout,
+  children: [{
+    path: 'index',
+    name: 'Form',
+    component: () =>
+                import('@/views/form/index'),
+    meta: { title: '条码打印', icon: 'form' }
+  }]
+},
 
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu2',
-    name: 'nested',
-    meta: {
-      title: '数据管理',
-      icon: 'nested'
-    },
-    children: [{
-      path: 'menu2',
-      component: () =>
-            import('@/views/nested/menu2/index'), // Parent router-view
-      name: 'Menu2',
-      meta: { title: '采购订单' },
-      children: []
-    }]
+{
+  path: '/nested',
+  component: Layout,
+  redirect: '/nested/menu2',
+  name: 'nested',
+  meta: {
+    title: '数据管理',
+    icon: 'nested'
   },
-  // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
+  children: [{
+    path: 'menu2',
+    component: () =>
+                import('@/views/nested/menu2/index'), // Parent router-view
+    name: 'Menu2',
+    meta: { title: '采购订单' },
+    children: []
+  }]
+},
+// 404 page must be placed at the end !!!
+{ path: '*', redirect: '/404', hidden: true }
 ]
 
 const createRouter = () => new Router({

14c4734cb93b105f6a470eea893d93d8938d229d
lijie <1262001568@qq.com>
2019-07-26 10:43:51 +0800

    fix：修改样式问题

--- src/views/stockout/stockout-Header.vue
+++ src/views/stockout/stockout-Header.vue
@@ -57,6 +57,8 @@
                   <el-table-column  prop="producedDate" label="生产日期" />
                   <el-table-column  label="操作" width="150">
                     <template slot-scope="scope">
+                      <el-button v-show="line.status == 1" type="text" size="small" @click="editLineFnc(scope.row)">编辑</el-button>
+                      <el-button v-show="line.status == 1" type="text" size="small" @click="deleteLineFnc(scope.row.id)">删除</el-button>
                       <el-button v-show="line.status == 3 || line.status == 2" type="text" size="small" @click="countingHandleClick(scope.row)">点收</el-button>
                       <el-button type="text" size="small" @click="turnToStockDetail(scope.row)">明细</el-button>
                     </template>
@@ -80,7 +82,7 @@
             </el-table-column>
             <el-table-column fixed="right" label="操作" width="200">
               <template slot-scope="scope">
-                <el-button v-show="scope.row.status==4" type="text" size="small" @click="confirmHandleClick(scope.row)">关闭</el-button>
+                <el-button v-show="scope.row.completed==true" type="text" size="small" @click="closeHandleClick(scope.row)">关闭</el-button>
                 <el-button v-show="scope.row.status==1" type="text" size="small" @click="LineHandleClick(scope.row)">添加单行</el-button>
                 <el-button v-show="scope.row.status==1" type="text" size="small" @click="confirmHandleClick(scope.row)">确认</el-button>
                 <el-button v-show="scope.row.status==1" type="text" size="small" @click="editHandleClick(scope.row)">编辑</el-button>
@@ -252,7 +254,7 @@
                     v-model="addLineData.itemId"
                     clearable
                     placeholder="物料名称"
-                    @change="itemChange"
+                    @change="itemAddChange"
                   >
                     <el-option
                       v-for="item in item"
@@ -334,8 +336,107 @@
     </div>
     <!--新增单行END-->
 
+      <!--编辑单行-->
+    <div>
+      <el-dialog title="编辑入库单行" :visible.sync="editLine">
+        <el-form ref="editLineData" :model="editLineData" class="demo-ruleForm">
+          <el-row>
+            <el-col :span="11">
+            <el-form-item
+                prop="itemId"
+                label="物料名称"
+                :label-width="formLabelWidth"
+                :rules="[
+                  { required: true, message: '物料名称不能为空'}
+                ]"
+              >
+                 <el-select
+                    v-model="editLineData.itemId"
+                    clearable
+                    placeholder="物料名称"
+                    @change="itemEditChange"
+                  >
+                    <el-option
+                      v-for="item in item"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col style="margin-left:10px" :span="11">
+           <el-form-item
+                prop="itemUnit"
+                label="入库单位"
+                :label-width="formLabelWidth"
+                :rules="[
+                  { required: true, message: '入库单位不能为空'}
+                ]"
+              >
+                 <el-select
+                    v-model="editLineData.itemUnit"
+                    clearable
+                    placeholder="入库单位"
+                  >
+                    <el-option
+                      v-for="item in unit"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.label"
+                    />
+                  </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+           <el-row>
+            <el-col :span="11">
+              <el-form-item
+                prop="batchNumber"
+                label="物料批次"
+                :label-width="formLabelWidth"
+                :rules="[
+                  { required: true, message: '物料批次不能为空'}
+                ]"
+              >
+                 <el-select
+                    v-model="editLineData.batchNumber"
+                    clearable
+                    placeholder="物料批次"
+                  >
+                    <el-option
+                      v-for="item in batchNumber"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.label"
+                    />
+                  </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col style="margin-left:10px" :span="11">
+              <el-form-item
+                prop="quantity"
+                label="入库数量"
+                :label-width="formLabelWidth"
+                :rules="[
+                  { required: true, message: '入库数量不能为空'},
+                ]"
+              >
+                <el-input v-model="editLineData.quantity" autocomplete="off" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
 
-    <!--编辑-->
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="editLine = false">取 消</el-button>
+          <el-button type="primary" @click="editLineHandleClick('editLineData')">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
+    <!---编辑单行END-->
+
+    <!--编辑单头-->
     <div>
       <el-dialog title="编辑入库单头" :visible.sync="edit">
         <el-form ref="editData" :model="editData" class="demo-ruleForm">
@@ -424,21 +525,23 @@ import {
   putHeader,
   deleteHeader,
   putHeaderStatus,
-  postLine
+  postLine,
+  putLine,
+  deleteLine
 } from "@/api/stock";
 import {getAllinvBatchList,getUnitAll,getItemAllFnc,getItemOne} from "@/api/baseData";
 export default {
-  name: "stockIn-Header",
+  name: "stockout-Header",
   data() {
     return {
       remote: [],
       loading:false,
       setRemote: [],
-      loading: false,
       add: false,
       edit: false,
       counting:false,//点收弹窗
       addLine:false, //新增单行弹窗
+      editLine:false,//编辑单行弹窗
       lineData: {},//行数据
       countingData:{    //点收数据
           quantity:'',
@@ -462,6 +565,7 @@ export default {
           batchNumber:'',
           quantity:''
       },
+      editLineData:[],
       editData: {},
       page: {
         // 查询条件
@@ -492,13 +596,74 @@ export default {
    this.getItemAllFnc();
   },
   methods: {
-    //选择物料下拉数据，用物料ID查询物料名称
-    itemChange(){
+    //删除单行数据
+    deleteLineFnc(e){
+      this.$confirm("此操作将永久删除该笔单行, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          deleteLine(e).then(res => {
+            if (res.errorCode === 0) {
+              this.add = false;
+              this.$message({
+                message: "删除成功",
+                type: "success"
+              });
+            }
+            this.fetchData();
+          });
+        })
+        .catch(() => {});
+    },
+    //编辑单行确定
+    editLineHandleClick(formName){
+         this.$refs[formName].validate(valid => {
+        if (valid) {
+          const param = {
+            id:this.editLineData.id,
+            itemName: this.editLineData.itemName,
+            itemUnit: this.editLineData.itemUnit,
+            batchNumber: this.editLineData.batchNumber,
+            quantity: this.editLineData.quantity
+          };
+          putLine(param).then(res => {
+            if (res.errorCode === 0) {
+              this.$message({
+                message: "编辑成功",
+                type: "success"
+              });
+              this.editLine = false;
+            }
+          });
+        } else {
+          this.$message.error("请完善信息!");
+          return false;
+        }
+      });
+    },
+    //编辑单行弹窗
+    editLineFnc(e){  
+      this.editLine = true; 
+      this.editLineData = e;
+    },
+    //单行新增选择物料下拉数据，用物料ID查询物料名称
+    itemAddChange(){
       let param = {
         id:this.addLineData.itemId
       }
        getItemOne(param).then(res => {
         this.addLineData.itemName = res.result.itemName;
+      });
+    },
+     //单行编辑选择物料下拉数据，用物料ID查询物料名称
+    itemEditChange(){
+      let param = {
+        id:this.editLineData.itemId
+      }
+       getItemOne(param).then(res => {
+        this.editLineData.itemName = res.result.itemName;
       });
     },
     //新增单行数据
@@ -558,6 +723,32 @@ export default {
               this.add = false;
               this.$message({
                 message: "单头确认成功",
+                type: "success"
+              });
+            }
+            this.fetchData();
+          });
+        })
+        .catch(() => {});
+    },  
+     //单头确认关闭
+    closeHandleClick(e){
+        let param = {
+            id:e.id,
+            status:5,
+            completed:false
+        }
+        this.$confirm("单头关闭后将不可再操作, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "info "
+      })
+        .then(async () => {
+          putHeaderStatus(param).then(res => {
+            if (res.errorCode === 0) {
+              this.add = false;
+              this.$message({
+                message: "单头关闭成功",
                 type: "success"
               });
             }
@@ -730,23 +921,22 @@ export default {
     //对某一行展开或者关闭的时候会触发该事件
     expandChange(row, expandedRows) {
       this.loading = true;
-      /*   if (expandedRows.length > 1) {
-          expandedRows.shift()
-        } */
       this.line.headerId=row.id;
       this.line.status = row.status;
-      sessionStorage.setItem("headerId", row.id);
+      //sessionStorage.setItem("headerId", row.id);
       this.getStockInLine();
     },
     //获取行数据
     getStockInLine(){
+      this.LineData = [];
       let params = {
-        headerId:sessionStorage.getItem("headerId")
+        headerId:this.line.headerId
       };
       getStockInLine(params).then(res => {
         this.LineData = res.result.list;
         this.loading = false;
       });
+      this.LineData = [];
     },
      //获取外部单据类型
     getTypeAllFnc() {
